200e4fc3b6d91303049cd71517725b1c4df333a1
Oskar Kellermann <oskar@kellermann.team>
2024-03-04 15:34:22 +0100

    responsive kunden sagen

--- lib/1_Landing.dart
+++ lib/1_Landing.dart
@@ -11,108 +11,152 @@ class Landing extends StatefulWidget {
 class _LandingState extends State<Landing> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
-            width: MediaQuery.of(context).size.width * 0.55,
-            height: MediaQuery.of(context).size.height,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 25, 25, 25),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.25,
-                ),
-                Text(
-                  'Erstklassige Frisuren',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: MediaQuery.of(context).size.width * 0.035,
-                    fontWeight: FontWeight.bold,
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return LayoutBuilder(
+      builder: (context, constraints) => SizedBox(
+        height: constraints.maxWidth <= 800 ? height * .7 : height,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
+              width: constraints.maxWidth <= 800
+                  ? width
+                  : MediaQuery.of(context).size.width * 0.55,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 25, 25, 25),
+              ),
+              child: Column(
+                crossAxisAlignment: constraints.maxWidth <= 800
+                    ? CrossAxisAlignment.center
+                    : CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.25,
                   ),
-                ),
-                Text(
-                  'direkt aus dem Barbershop',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: MediaQuery.of(context).size.width * 0.035,
-                    fontWeight: FontWeight.bold,
+                  Text(
+                    'Erstklassige Frisuren\ndirekt aus dem Barbershop',
+                    textAlign: constraints.maxWidth <= 800
+                        ? TextAlign.center
+                        : TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: constraints.maxWidth <= 800
+                          ? width * .07
+                          : MediaQuery.of(context).size.width * 0.035,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.03,
-                ),
-                Text(
-                  'Get the perfect cut every time of your dreams at',
-                  style: TextStyle(
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                  ),
+                  Text(
+                    'Get the perfect cut every time of your dreams at\nthe barber shop that cares about your style',
+                    style: TextStyle(
                       color: Colors.white,
-                      fontSize: MediaQuery.of(context).size.width * 0.02,
-                      fontWeight: FontWeight.w100),
-                ),
-                Text(
-                  'the barber shop that cares about your style',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: MediaQuery.of(context).size.width * 0.02,
-                      fontWeight: FontWeight.w100),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.175,
-                      height: MediaQuery.of(context).size.height * 0.05,
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: Colors.white),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Buchen',
-                          style: TextStyle(color: Colors.white),
+                      fontSize: constraints.maxWidth <= 800
+                          ? width * .045
+                          : MediaQuery.of(context).size.width * 0.02,
+                      fontWeight:
+                          constraints.maxWidth <= 800 ? null : FontWeight.w100,
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  constraints.maxWidth <= 800
+                      ? Column(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              height: MediaQuery.of(context).size.height * 0.05,
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 1, color: Colors.white),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  'Buchen',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.width * 0.025,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              height: MediaQuery.of(context).size.height * 0.05,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 1, color: main.primaryColor),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '+43 681 10595141',
+                                  style: TextStyle(color: main.primaryColor),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      : Row(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.18,
+                              height: MediaQuery.of(context).size.height * 0.05,
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 1, color: Colors.white),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  'Buchen',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.025,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.18,
+                              height: MediaQuery.of(context).size.height * 0.05,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 1, color: main.primaryColor),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '+43 681 10595141',
+                                  style: TextStyle(color: main.primaryColor),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.025,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.175,
-                      height: MediaQuery.of(context).size.height * 0.05,
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: main.primaryColor),
-                      ),
-                      child: Center(
-                        child: Text(
-                          '+43 681 10595141',
-                          style: TextStyle(color: main.primaryColor),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * .09,
-            ),
-            child: Image.network(
-              'https://images.pexels.com/photos/2068672/pexels-photo-2068672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            ),
-          ),
-        ],
+            constraints.maxWidth <= 800
+                ? const SizedBox()
+                : Padding(
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * .09,
+                    ),
+                    child: Image.network(
+                      'https://images.pexels.com/photos/2068672/pexels-photo-2068672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                      width: MediaQuery.of(context).size.width * .4,
+                    ),
+                  ),
+          ],
+        ),
       ),
     );
   }
--- lib/4_Testimonials.dart
+++ lib/4_Testimonials.dart
@@ -12,104 +12,73 @@ class Testimoials extends StatefulWidget {
 class _TestimoialsState extends State<Testimoials> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.05),
-      height: MediaQuery.of(context).size.height * 0.6,
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            alignment: Alignment.topLeft,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(20),
-                height: MediaQuery.of(context).size.height * 0.4,
-                width: MediaQuery.of(context).size.width * 0.3,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: main.primaryColor),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(color: main.greyZero),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.1,
-          ),
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.17,
+    return LayoutBuilder(
+      builder: (context, constraints) => Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.045,
+        ),
+        height: MediaQuery.of(context).size.height * 0.7,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            constraints.maxWidth <= 800
+                ? const SizedBox()
+                : Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: main.primaryColor),
+                    ),
+                    child: Image.network(
+                      'https://images.pexels.com/photos/3993472/pexels-photo-3993472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                      height: MediaQuery.of(context).size.height * .5,
+                    ),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        'Ein Kunde sagt',
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.1,
+            ),
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.17),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Eine Kundin sagt',
+                          style: TextStyle(
+                            color: main.primaryColor,
+                            fontSize: MediaQuery.of(context).size.width *
+                                (constraints.maxWidth <= 800 ? .08 : .035),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height *
+                          (constraints.maxWidth <= 800 ? .02 : .04),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width *
+                          (constraints.maxWidth <= 800 ? .8 : .4),
+                      child: Text(
+                        'I was very happy because I chose my hairstyle here. The barber is very friendly, gives excellent treatment and has great techniques to choose a suitable hairstyle for your hair. I advise everyone in the town or tourists to choose a new haircut here.\n- Laura Müller',
                         style: TextStyle(
-                          color: main.primaryColor,
-                          fontSize: 50,
+                          color: const Color.fromARGB(255, 179, 179, 179),
+                          fontSize: MediaQuery.of(context).size.width *
+                              (constraints.maxWidth <= 800 ? .04 : .0125),
                         ),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.04,
-                  ),
-                  const Text(
-                    'I was very happy because I chose my hairstyle here.',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 179, 179, 179),
                     ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
-                  ),
-                  const Text(
-                    'The barber is very friendly, gives excellent treatment and has great ',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 179, 179, 179),
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
-                  ),
-                  const Text(
-                    'techniques to choose a suitable hairstyle for your hair. ',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 179, 179, 179),
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
-                  ),
-                  const Text(
-                    'I advise everyone in the town or tourists to choose a new haircut here.',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 179, 179, 179),
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.025,
-                  ),
-                  const Text(
-                    '- Adam Smith',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          )
-        ],
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
